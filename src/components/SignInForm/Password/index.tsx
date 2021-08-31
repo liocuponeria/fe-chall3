bf7f907bafe1a0e1e65db107e690f97f2bb86f9f
mb4ndeira <mbandeira.cs@gmail.com>
2021-08-31 13:37:47 -0300

    fix: signin forms can't find styles.ts

--- src/components/SignInForm/Password/index.tsx
+++ src/components/SignInForm/Password/index.tsx
@@ -2,14 +2,12 @@ import React from 'react'
 
 import Input from '../../Input'
 
-import { Container } from './styles'
-
 const Contact: React.FC = () => {
   return (
-    <Container>
+    <div>
       <Input name="email" placeholder="e-mail" />
       <Input name="password" placeholder="Criar senha" />
-    </Container>
+    </div>
   )
 }
 
--- src/components/SignInForm/Start/index.tsx
+++ src/components/SignInForm/Start/index.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 
 import Input from '../../Input'
 
-import { Container } from './styles'
-
 const Contact: React.FC = () => {
   return (
-    <Container>
+    <div>
       <Input name="name" placeholder="Nome" />
       <Input name="family_name" placeholder="Sobrenome" />
       <Input name="phone" placeholder="Telefone" />
-    </Container>
+    </div>
   )
 }
 
