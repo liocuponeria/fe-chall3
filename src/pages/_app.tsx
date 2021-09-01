9076159cd6a67bb4cec5e629dbdd87f8e4fbd82f
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-01 04:27:07 -0300

    feat: integrate cart context to application

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,15 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 
+import { CartProvider } from '../contexts/Cart'
+
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyle />
+      <CartProvider>
+        <Component {...pageProps} />
+        <GlobalStyle />
+      </CartProvider>
     </ThemeProvider>
   )
 }
