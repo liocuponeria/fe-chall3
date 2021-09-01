6c7814ccc6b38d9b8e628d1335e0c16ea7d73619
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-01 02:19:02 -0300

    feat: add onChange prop to input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,10 +8,20 @@ interface InputProps {
   error?: boolean
 }
 
-const Input: React.FC<InputProps> = ({ name, placeholder = 'test', error }) => {
+const Input: React.FC<InputProps> = ({
+  name,
+  placeholder = 'test',
+  onChange,
+  error
+}) => {
   return (
     <Container error={error}>
-      <input type="text" name={name} placeholder={placeholder} />
+      <input
+        type="text"
+        name={name}
+        placeholder={placeholder}
+        onChange={e => onChange(e.target.value)}
+      />
     </Container>
   )
 }
