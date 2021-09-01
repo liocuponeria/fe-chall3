3c6e2b527e6fb46bf5d1c287db22357655682b44
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-01 13:59:03 -0300

    refactor: add to cart function on context

--- src/contexts/Cart.tsx
+++ src/contexts/Cart.tsx
@@ -1,5 +1,4 @@
 import React, { createContext, ReactNode, useState } from 'react'
-import { useRouter } from 'next/router'
 
 import CartType from '../types/Cart'
 import ProductType from '../types/Product'
@@ -17,42 +16,49 @@ interface CartProviderProps {
 const CartContext = createContext({} as CartContextData)
 
 const CartProvider = ({ children }: CartProviderProps) => {
-  const router = useRouter()
-
   const [cart, setCart] = useState<CartType>([])
 
   const addToCart = (product: ProductType) => {
     const { id, image, title, price } = product
-
     const productToInsert = { quantity: 1, id, image, title, price }
-    const newCart = cart || ([] as CartType)
 
-    if (newCart !== []) {
-      const matchingProductIndex = newCart.findIndex(
+    let newCart = cart || ([] as CartType)
+
+    console.log(cart)
+
+    if (!cart || cart.length === 0) newCart = [productToInsert]
+
+    let matchingProductIndex = -1
+
+    console.log(cart)
+
+    if (cart.length > 0) {
+      matchingProductIndex = cart.findIndex(
         cartProduct => cartProduct.id === product.id
       )
+    }
 
-      if (matchingProductIndex >= 0) {
-        newCart[matchingProductIndex] = {
-          ...productToInsert,
-          quantity: newCart[matchingProductIndex].quantity + 1
-        }
-      } else {
-        newCart.push(productToInsert)
+    console.log(matchingProductIndex)
+
+    if (matchingProductIndex >= 0) {
+      newCart[matchingProductIndex] = {
+        ...productToInsert,
+        quantity: newCart[matchingProductIndex].quantity + 1
       }
     }
 
-    setCart(newCart)
+    if (matchingProductIndex < 0 && cart.length > 0) {
+      console.log('true')
+      newCart.push(productToInsert)
+    }
 
-    router.push('/signin')
+    setCart(newCart)
   }
 
   const removeFromCart = (id: number) => {
     const newCart = cart.filter(product => product.id !== id)
 
     setCart(newCart)
-
-    if (!cart) router.push('/')
   }
 
   return (
