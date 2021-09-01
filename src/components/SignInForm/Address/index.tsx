8a850b52b3c100c3751efcdb3159862e0b3f6f60
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-01 02:19:51 -0300

    feat: add address auto-completion to signin form

--- src/components/SignInForm/Address/index.tsx
+++ src/components/SignInForm/Address/index.tsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+
+import CEPAPI from '../../../services/CEP'
 
 import Input from '../../Input'
 
 const Contact: React.FC = () => {
+  const formRef = useRef<HTMLDivElement>(null)
+
+  const [CEPHasError, setCEPHasError] = useState<boolean>(false)
+
+  const handleCEPChange = async (CEP: string) => {
+    try {
+      const { localidade, logradouro } = await CEPAPI.get(
+        `/ws/${CEP}/json/`
+      ).then(response => response.data)
+
+      formRef.current.children[1].children[0].value = logradouro
+      formRef.current.children[2].children[0].value = localidade
+      formRef.current.children[3].children[0].value = 'Brasil'
+    } catch (error) {
+      console.error(error)
+
+      setCEPHasError(true)
+    }
+  }
+
   return (
-    <div>
-      <Input name="CEP" placeholder="CEP" />
+    <div ref={formRef}>
+      <Input
+        name="CEP"
+        placeholder="CEP"
+        onChange={handleCEPChange}
+        error={CEPHasError}
+      />
       <Input name="street" placeholder="Rua" />
       <Input name="city" placeholder="Cidade" />
       <Input name="country" placeholder="País" />
