e31f4fb70e55ba859142a57a97ec92565877e41b
mb4ndeira <mbandeira.cs@gmail.com>
2021-09-01 04:25:36 -0300

    feat: set interface and populate product component

--- src/components/Product/index.tsx
+++ src/components/Product/index.tsx
@@ -4,26 +4,29 @@ import Button from '../Button'
 
 import { Container } from './styles'
 
-const Product: React.FC<React.HTMLAttributes<HTMLDivElement>> = ({
-  className
-}) => {
+import ProductType from '../../types/Product'
+
+interface ProductProps {
+  product: ProductType
+}
+
+const Product: React.FC<
+  ProductProps & React.HTMLAttributes<HTMLDivElement>
+> = ({ className, product }) => {
+  const { image, title, price, description } = product
+
   return (
     <Container className={className}>
       <div>
-        <img src="https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80" />
+        <img src={image} />
       </div>
       <div>
         <div>
-          <h3>Boné Bordado Harry Potter Grifinoria</h3>
-          <span>R$30,00</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            necessitatibus similique molestias quis minima maxime. Lorem ipsum
-            dolor sit amet consectetur adipisicing elit. Magnam veritatis
-            corrupti fugiat, hic molestias odit?
-          </p>
+          <h3>{title}</h3>
+          <span>R${price}</span>
+          <p>{description}</p>
         </div>
-        <Button text="Adicionar ao carrinho" />
+        <Button text="Adicionar ao carrinho" href="/signin" />
       </div>
     </Container>
   )
