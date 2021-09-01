6b7e978f491e3c9c98cc4dc46b20e516db2ffc01
mb4ndeira <mbandeira.cs@gmail.com>
2021-08-31 22:37:12 -0300

    feat: add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Head from 'next/head'
+
+import {
+  Container,
+  StyledHeader,
+  StyledForm,
+  StyledNavBar,
+  StyledFooter
+} from '../styles/pages/LogIn'
+
+const SignIn: React.FC = () => {
+  return (
+    <Container>
+      <Head>
+        <title>Login</title>
+      </Head>
+
+      <StyledHeader />
+      <StyledForm />
+      <StyledFooter />
+      <StyledNavBar />
+    </Container>
+  )
+}
+
+export default SignIn
---
+++ src/styles/pages/LogIn.ts
@@ -0,0 +1,75 @@
+import styled from 'styled-components'
+
+import Header from '../../components/Header'
+import LogInForm from '../../components/LogInForm'
+import NavBar from '../../components/NavBar'
+import Footer from '../../components/Footer'
+
+export const Container = styled.main`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-template-rows: 55px 1fr 120px;
+
+  grid-template-areas:
+    'header'
+    'form'
+    'footer';
+
+  position: relative;
+
+  @media (min-width: 486px) {
+    grid-template-rows: 60px 1fr 120px;
+  }
+
+  @media (min-width: 720px) {
+    grid-template-rows: 80px 1fr 120px;
+  }
+
+  @media (min-width: 1024px) {
+    grid-template-rows: 100px 1fr 120px;
+  }
+`
+
+export const StyledHeader = styled(Header)`
+  grid-area: header;
+`
+export const StyledForm = styled(LogInForm)`
+  grid-area: form;
+  justify-self: center;
+
+  margin: 20px 18px 14px 18px;
+
+  width: 100%;
+
+  @media (min-width: 826px) {
+    margin-top: calc(55px + 30px);
+    margin-bottom: 100px;
+  }
+`
+
+export const StyledFooter = styled(Footer)`
+  grid-area: footer;
+
+  @media (max-width: 826px) {
+    margin-bottom: -55px;
+  }
+`
+
+export const StyledNavBar = styled(NavBar)`
+  position: fixed;
+  bottom: 0;
+
+  height: 55px;
+
+  @media (min-width: 826px) {
+    position: absolute;
+    top: 80px;
+  }
+
+  @media (min-width: 1024px) {
+    position: absolute;
+    top: 100px;
+
+    height: 60px;
+  }
+`
